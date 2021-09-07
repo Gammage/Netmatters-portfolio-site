fabbbb4163ecde204dff41fbc6f061f6577e8cca
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-09-07 15:17:17 +0100

    Update head.php

--- public/assets/components/head.php
+++ public/assets/components/head.php
@@ -1,7 +1,7 @@
 <head>
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="../public/assets/css/hamburgers.min.css">
-<link rel="stylesheet" href="../public/assets/css/styles.css">
+<link rel="stylesheet" href="./assets/css/hamburgers.min.css">
+<link rel="stylesheet" href="./assets/css/styles.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
