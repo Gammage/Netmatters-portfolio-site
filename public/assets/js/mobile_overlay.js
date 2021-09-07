d3428845374d93057aca65f074a728f255b0f1f2
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-09-07 21:23:04 +0100

    Update mobile_overlay.js

--- public/assets/js/mobile_overlay.js
+++ public/assets/js/mobile_overlay.js
@@ -22,29 +22,28 @@ function buttonToggle() {
     if (switchToggle) {
         console.log("yes");
 
-        for (let i = 0; i < hamburgers.length; i = i + 1) {
-            console.log(hamburgers[i]);
-            hamburgers[i].classList.add("is-active");
+        for (let i = 0; i < burger.length; i = i + 1) {
+            console.log(burger[i]);
+            burger[i].classList.add("is-active");
         }
 
         sidenav.style.display = "block";
         main.classList.add("freeze");
         overlay.style.display = "block";
         body.classList.add("hideme");
+
     } else {
         console.log("no");
 
-        for (let i = 0; i < hamburgers.length; i = i + 1) {
-            console.log(hamburgers[i]);
-            hamburgers[i].classList.remove("is-active");
+        for (let i = 0; i < burger.length; i = i + 1) {
+            console.log(burger[i]);
+            burger[i].classList.remove("is-active");
         }
-
 
         overlay.style.display = "none";
         main.classList.remove("freeze");
         sidenav.style.display = "none";  
         overlay.style.display = "none";
         body.classList.remove("hideme");
-
     };
 }
