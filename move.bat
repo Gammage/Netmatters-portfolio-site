1765bc4b74b0e66864e3920caae4406ba8a32687
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-09-07 16:38:01 +0100

    Update move.bat

--- move.bat
+++ move.bat
@@ -1,3 +1,3 @@
 @RD /S /Q "C:\xampp\htdocs"
 mkdir "C:\xampp\htdocs"
-xcopy "C:\Users\Benja\OneDrive\Documents\GitHub\Netmatters-portfolio-site\public" "C:\xampp\htdocs\public" /E
+xcopy "C:\Users\Benja\OneDrive\Documents\GitHub\Netmatters-portfolio-site\public" "C:\xampp\htdocs\" /E
