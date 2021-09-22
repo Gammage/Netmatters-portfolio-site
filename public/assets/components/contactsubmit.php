3edc25531635033719d1d3a994698e64aa63423a
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-09-22 14:27:09 +0100

    Update contactsubmit.php

--- public/assets/components/contactsubmit.php
+++ public/assets/components/contactsubmit.php
@@ -14,9 +14,9 @@ $messagevalid = false;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "netmatters_portfolio_site";
+    $username = "bengamma_bengammage";
+    $password = "B_202003195502";
+    $dbname = "bengammage_netmatters_portfolio_site";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
